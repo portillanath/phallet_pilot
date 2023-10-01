a9b5b13921952f3d8865789ffb478bc162820fb0
portillanath <na.portilla10>
2023-09-30 21:26:17 -0500

    New ICTV changes

--- 00.ICTV_Viral_Resource.sh
+++ 00.ICTV_Viral_Resource.sh
@@ -1,43 +1,36 @@
-#Following the steps on this script update the ICTV viral Resource taxonomy classification 
+#!/bin/bash
 
-#First step is intall crontab as a conda enviroment 
-conda create -n crocrontab -lcrontab -lntab
-conda activate crontab
-#Installation thorugh bioconda 
-conda install -c conda-forge crontab
+# Conda Environment Creation
+conda create -n crocrontab -c conda-forge crontab
 
-#Now copy the personal bashrc 
-nano ~/.bashrc 
-#Create a new bashr to path the crontab 
+# Activate Conda Environment
+conda activate crocrontab
+
+# Copy Personal Bashrc
+nano ~/.bashrc
+
+# Create a new bashrc to Path the Conda Environment
 nano ~/.bashrc_conda
 
-#The following script set up the new root 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/hpcfs/home/ciencias_biologicas/na.portilla10/anaconda3_install/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/hpcfs/home/ciencias_biologicas/na.portilla10/anaconda3_install/etc/profile.d/conda.sh" ]; then
-        . "/hpcfs/home/ciencias_biologicas/na.portilla10/anaconda3_install/etc/profile.d/conda.sh"
-    else
-        export PATH="/hpcfs/home/ciencias_biologicas/na.portilla10/anaconda3_install/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
+# Content for ~/.bashrc_conda
+echo 'export CONDA_HOME="/path/to/anaconda3_install"' >> ~/.bashrc_conda
+echo 'export CARGO_HOME="$HOME/.cargo"' >> ~/.bashrc_conda
+echo 'export PATH="$CONDA_HOME/bin:$CARGO_HOME/bin:$PATH"' >> ~/.bashrc_conda
+source ~/.bashrc_conda
 
-. "$HOME/.cargo/env"
+#Create a Virus Metadata Resource
+mkdir -p Virus_Metadata_Resource 
 
-#After this, a new crontab job is made 
-crontab -e
+# After this, a new crontab job is made
+crontab -e 
 
-SHELL=/bin/bash
-BASH_ENV=~/.bashrc_conda
+# Content for Crontab
+echo 'SHELL=/bin/bash' > crontab_script
+echo 'BASH_ENV=~/.bashrc_conda' >> crontab_script
+30 12 * * * wget https://ictv.global/vmr/current > ./Virus_Metadata_Resource | rm `ls -t | awk 'NR>1'`
+crontab crontab_script
 
-30 12 * * * wget https://ictv.global/vmr/current > /hpcfs/home/ciencias_biologicas/na.portilla10/Source_PhageClouds/Virus_Metadata_Resource | rm `ls -t | awk 'NR>1'`
-
-#To check the status 
+# Check Crontab Status
 crontab -l
 
 

82452bb2e5ada5dbdfac430da4ca256f83b5ec8e
portillanath <na.portilla10>
2023-11-09 15:10:23 -0500

    Dependencies actualization

--- dependencies.sh
+++ dependencies.sh
@@ -4,7 +4,7 @@ cd $workdir
 #This Script is made for install all dependencies
 
 #Install and create all conda enviroments 
-conda create --name taxa_curation --file taxa_curation.yaml --no-prefix
-conda create --name blast_feed --file blast_feed.yaml --no-prefix
-conda create --name ANI --file ANI.yaml --no-prefix
-conda create --name wraggling_metrics --file wraggling_metrics.yaml --no-prefix
+conda create --name taxa_curation --file taxa_curation.yaml 
+conda create --name blast_feed --file blast_feed.yaml 
+conda create --name ANI --file ANI.yaml 
+conda create --name wraggling_metrics --file wraggling_metrics.yaml

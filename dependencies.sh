376b1204f4f4e667e4ff757f698d91825f84d291
portillanath <na.portilla10>
2023-11-10 11:51:44 -0500

    Update dependeintes

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-workdir="./enviroments"
-cd $workdir || exit
 
-#This Script is made for install all dependencies
+workdir="./enviroments"
+cd "$workdir" || exit
+
+# Print the current working directory
 pwd
 
-#Install and create all conda enviroments 
-conda create --name taxa_curation --file taxa_curation.yaml 
-conda create --name blast_feed --file blast_feed.yaml 
-conda create --name ANI --file ANI.yaml 
-conda create --name wraggling_metrics --file wraggling_metrics.yaml
+# Array of YAML files
+yaml_files=("taxa_curation.yaml" "blast_feed.yaml" "ANI.yaml" "wraggling_metrics.yaml")
+
+# Loop through YAML files and create Conda environments
+for file in "${yaml_files[@]}"; do
+  # Check if the file exists
+  if [ -f "$file" ]; then
+    # Extract environment name from YAML file using basename
+    env_name=$(basename "$file" .yaml)
+    # Print information
+    echo "Creating Conda environment '$env_name' from '$file'"
+    # Create Conda environment
+    conda env create --name "$env_name" --file "$file"
+  else
+    echo "File not found: $file"
+  fi
+done
+

848c0d3d2f7fddfb907748b795e7a2168ad67b74
portillanath <na.portilla10>
2023-11-27 11:23:00 -0500

    Changes on graphing panel

--- 07.Graphing.py
+++ 07.Graphing.py
@@ -9,7 +9,8 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 # Set working directory
 workdir = os.path.expanduser("~/phallet/Metrics_Results")
-subdirectories=[d for d in os.listdir(workdir)]
+subdirectories = [os.path.join(workdir, name) for name in os.listdir(workdir) if os.path.isdir(os.path.join(workdir, name))]
+print(subdirectories)
 
 # Read command-line arguments
 args = sys.argv[1:]
@@ -46,62 +47,84 @@ elif mx == "vcontact2":
 
 metrics = [mx, my]
 
-ani_data=pd.DataFrame()
-mash_data=pd.DataFrame()
-viridic_data=pd.DataFrame()
+ani_data = pd.DataFrame()
+mash_data = pd.DataFrame()
+viridic_data = pd.DataFrame()
+mx_data = pd.DataFrame()
+my_data = pd.DataFrame()
+df_subset_mx = pd.DataFrame()
+df_subset_my = pd.DataFrame()
+kmer_values_mx = []
+kmer_values_my = []
 
 for m in metrics:
-        if m==mx:
-            kmers=kmersx
-            tool_mx=tool_mx
-        if m==my:
-            kmers=kmersy
-            tool_my=tool_my
-# Retrieve information for each metric
+    if m == mx:
+        kmers = kmersx
+        tool_mx = tool_mx
+    if m == my:
+        kmers = kmersy
+        tool_my = tool_my
 
 for genus in subdirectories:
-    os.chdir(workdir)
+    os.chdir(genus)
+    print(os.getcwd())
     genus_name = os.path.basename(genus)
-    genus_dir= os.path.join(workdir,genus_name)
-    summaries = glob.glob(os.path.join(genus_dir,"*metrics*.csv"))
+    genus_dir = os.path.join(workdir, genus_name)
+    summaries = glob.glob(os.path.join(genus_dir, "*metrics*.csv"))
 
     if mx in metrics:
-        mx_data = pd.read_csv([s for s in summaries if f"{mx}_metrics_" in s][0])
-        kmer_values_mx = sorted(pd.unique(mx_data[f"kmer_{mx}"]))
-    
+        mx_files = [s for s in summaries if f"{mx}_metrics_" in s]
+        if len(mx_files) > 0:
+            mx_data = pd.read_csv(mx_files[0])    
+        else:
+            print(f"No {mx} data for {genus_name}")
     if my in metrics:
-        my_data = pd.read_csv([s for s in summaries if f"{my}_metrics_" in s][0])
-        kmer_values_my = sorted(pd.unique(my_data[f"kmer_{my}"]))
-        
+        my_files = [s for s in summaries if f"{my}_metrics_" in s]
+        if len(my_files) > 0:
+            my_data = pd.read_csv(my_files[0])
+        else:
+            print(f"No {my} data for {genus_name}")
+            
 # Assuming merged_df is your DataFrame
-for i, algorithm_mx in enumerate(tool_mx):
-    for j, algorithm_my in enumerate(tool_my):
+        for algorithm_mx in tool_mx:
+            for algorithm_my in tool_my:
+            # Subset the data for the current combination
+              if "algorithm" in mx_data.columns:
+                df_subset_mx = mx_data[mx_data["algorithm"] == algorithm_mx] 
+                kmer_values_mx = sorted(pd.unique(df_subset_mx[f"kmer_{mx}"])) 
 
-        # Subset the data for the current combination
-        df_subset_mx = mx_data[mx_data["algorithm"]==algorithm_mx]
-        df_subset_my = my_data[my_data["algorithm"]==algorithm_my]
+              if "algorithm" in my_data.columns:
+                df_subset_my = my_data[my_data["algorithm"] == algorithm_my]
+                kmer_values_my = sorted(pd.unique(df_subset_my[f"kmer_{my}"]))
+   
+# Create a scatterplot for each kmer combination
+              fig, axes = plt.subplots(len(kmer_values_mx), len(kmer_values_my), figsize=(12,8))
+              axes = axes.reshape(len(kmer_values_mx), len(kmer_values_my))
+# Check if there are any valid combinations of kmer_values_mx and kmer_values_my
+              fig.text(0.04, 0.04, algorithm_my, va='center', rotation='vertical')
 
-        # Create a scatterplot for each kmer combination
-        for mx_kmer in kmer_values_mx:
-            for my_kmer in kmer_values_my:
-
-                # Subset the data for the current kmer_mx and kmer_my
-                df_subset_mx_kmer = df_subset_mx[df_subset_mx[f"kmer_{mx}"] == mx_kmer]
-                df_subset_my_kmer = df_subset_my[df_subset_my[f"kmer_{my}"] == my_kmer]
-                merged_df = pd.merge(df_subset_my_kmer, df_subset_mx_kmer, left_on="GenomeA", right_on="GenomeB")
-
-                # Create a scatterplot for the current kmer_mx and kmer_my
-                fig, ax = plt.subplots(figsize=(8, 6))
-                ax.scatter(merged_df.iloc[:, 7], merged_df.iloc[:, 2])
-                ax.axvline(x=95, color="black")
-                ax.set_xlabel(mx)
-                ax.set_ylabel(my)
-                ax.set_title(f"{algorithm_mx}, {algorithm_my}, {mx_kmer}, {my_kmer}")
-
-                # Save the current figure to a PDF file
-                pdf_filename = f"{algorithm_mx}_{algorithm_my}_{mx_kmer}_{my_kmer}.pdf"
-                with PdfPages(pdf_filename) as pdf:
-                    pdf.savefig(fig)
-
-# Show a message when the PDF files have been created
-print("PDF files created successfully.")
+              for i, mx_kmer in enumerate(kmer_values_mx):
+                 for j, my_kmer in enumerate(kmer_values_my):
+        # Subset the data for the current kmer_mx and kmer_my
+                    df_subset_mx_kmer = df_subset_mx[df_subset_mx[f"kmer_{mx}"] == mx_kmer] 
+                    df_subset_my_kmer = df_subset_my[df_subset_my[f"kmer_{my}"] == my_kmer]
+                    # Check if there are any data points for the current kmer combination
+                    merged_df = pd.merge(df_subset_mx_kmer, df_subset_my_kmer, on=["GenomeA", "GenomeB"], how="outer")
+                    print(merged_df)
+                    print(os.getcwd())
+        # Create a scatterplot for the current kmer_mx and kmer_my
+                    ax = axes[i, j]
+                    ax.set_title(f"{algorithm_mx}:{mx_kmer},{algorithm_my}:{my_kmer}",fontsize=8)
+                    ax.scatter(merged_df[f"{mx}_distance"],merged_df[f"{my}_distance"],alpha=0.5,s=3)
+                          # Remove y-axis labels from subplots except for the leftmost column
+                    if j > 0:
+                       ax.set_yticklabels([])
+        
+        # Set a single y-label scale on the left side of the panel
+                    if j == 0:
+                       ax.set_ylabel(algorithm_my, rotation=0, ha='right', va='center')
+                       ax.yaxis.set_label_coords(-0.2, 0.5)                            
+        # Save the current figure to a PDF file if there are valid combinations
+              pdf_filename = f"{genus_name}_{algorithm_mx}_{algorithm_my}.pdf"
+              with PdfPages(pdf_filename) as pdf:
+                  pdf.savefig(fig)

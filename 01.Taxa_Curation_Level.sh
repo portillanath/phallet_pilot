e938f1bb1dcea842914dd56e581766aeeca198fc
portillanath <na.portilla10>
2023-11-27 18:51:20 -0500

    Solving conda initit

--- 01.Taxa_Curation_Level.sh
+++ 01.Taxa_Curation_Level.sh
@@ -3,9 +3,9 @@ workdir=~/phallet
 cd $workdir 
 
 #Create a directory for taxa selected
-
+eval "$(conda shell.bash hook)"
 mkdir -p Taxa_Selected
-source activate taxa_curation 
+conda activate taxa_curation 
 for genus_name in "$@";do
 python3 01.Taxa_Curation_Level.py $genus_name
 done
--- dependencies.sh
+++ dependencies.sh
@@ -8,32 +8,11 @@ cd "$workdir" || exit
 # Print the current working directory
 pwd
 
-# Get the path to the conda executable
-CONDA_PATH=$(which conda)
-echo "Conda path: $CONDA_PATH"
-
-# Check if conda is installed
-if [ -z "$CONDA_PATH" ]; then
-    echo "Conda is not installed or not in the system PATH."
-    exit 1
-fi
-
-# Set the default environment manager
-DEFAULT_MANAGER="conda"
-
-# Add the conda command to the .bashrc file
-echo "alias conda='$CONDA_PATH'" >> ~/.bashrc
-
-# Set the default environment manager in the .bashrc file
-echo "export CONDA_DEFAULT_ENV_MANAGER=$DEFAULT_MANAGER" >> ~/.bashrc
-
-# Activate the changes in the current shell session
-source ~/.bashrc
-
 # Array of YAML files
 
 yaml_files=("taxa_curation.yaml" "blast_feed.yaml" "ANI.yaml" "wraggling_metrics.yaml" "mash.yaml")
 
+eval "$(conda shell.bash hook)"
 # Loop through YAML files and create Conda environments
 for file in "${yaml_files[@]}"; do
   # Check if the file exists

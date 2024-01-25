f4ba0e3a58ce207da247f74f934c3fc17f2c7bb5
portillanath <na.portilla10>
2024-01-24 19:24:52 -0500

    Changes on time managment

--- phallet.sh
+++ phallet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 data_default=~/phallet/test_genus.txt
 #Run phallet steps 
-bash 00.ICTV_Metadata_Resource.sh
-bash 01.Taxa_Curation_Level.sh $data_default
-bash 02.Blast_Feed.sh 
-bash 03.Merge_blast.sh 
-bash 04.Mash_Metrics.sh
-bash 05.ANI_Metrics.sh 
-bash 06.wraggling.sh
-bash 07.Graphing.sh
+time bash 00.ICTV_Metadata_Resource.sh
+time bash 01.Taxa_Curation_Level.sh $data_default
+time bash 02.Blast_Feed.sh 
+time bash 03.Merge_blast.sh 
+time bash 04.Mash_Metrics.sh
+time bash 05.ANI_Metrics.sh 
+time bash 06.wraggling.sh
+time bash 07.Graphing.sh
 
